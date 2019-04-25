8411a61ff471c23be69edd93569f02138d8efdb4
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-04-25 23:59:18 +0530

    display hello world with line break.
    display hello world with line break.

---
+++ cpp test 2.cpp
@@ -0,0 +1,15 @@
+//oshe ravanz contribution
+//oshada97
+#include <iostream>
+using namespace std;
+
+int main() //main function everything happend inside of this
+{
+    cout << "Hello world!"<<endl; //"cout" function is a use to print,display output ,"endl" is a line break
+	
+    return 0; //programme stop command,exit status or exit message for code
+}
+
+
+
+ 

81912bf56980da4e73b604f716dcf8c60c9f6473
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-04-25 23:59:39 +0530

    Update cpp test 1.cpp

--- cpp test 1.cpp
+++ cpp test 1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() //main function everything happend inside of this
 {
-    cout << "Hello world!"; //cout function is a use to print,display output
+    cout << "Hello world!"; //"cout" function is a use to print,display output
 	
     return 0; //programme stop command,exit status or exit message for code
 }

fb3418950b957aa961c7d981f86ac8b8bca5732d
Oshada Basnayake <oshadasinhalaya@gmail.com>
2019-04-25 23:55:49 +0530

    display hello world
    display hello world without line breaks.

---
+++ cpp test 1.cpp
@@ -0,0 +1,15 @@
+//oshe ravanz contribution
+//oshada97
+#include <iostream>
+using namespace std;
+
+int main() //main function everything happend inside of this
+{
+    cout << "Hello world!"; //cout function is a use to print,display output
+	
+    return 0; //programme stop command,exit status or exit message for code
+}
+
+
+
+ 
